b20c6c81acd3795b59b9cd5034dee629c7ebfcd5
Laurence-W <laurenceother@hotmail.com>
2022-12-17 18:58:44 +1100

    added exercise removal feature

--- venv/exercisesimport.py
+++ venv/exercisesimport.py
@@ -27,11 +27,50 @@ showexerciselist()
     shutil.move(tempfile.name, filename)
 removeexercise()'''
 
-def removeexercise():
-    removed_exercise = input("Please type out the exercise to remove:")
+'''def removeexercise():
+    removed_exercise = str(input("Please type out the exercise to remove:"))
     with open('/home/laurence/test/exercises.csv', 'rb') as inp, open('/home/laurence/test/exercises_edited.csv', 'wb') as out:
         writer = csv.writer(out)
         for row in csv.reader(inp):
             if row[1] != removed_exercise:
                 writer.writerow(row)
+removeexercise()'''
+
+'''def d_avatar():
+    filePath = "/home/laurence/test/exercises.csv"
+    with open(filePath)	as csvfile:
+        reader = csv.DictReader(csvfile)
+        
+        filePath = "/home/laurence/test/exercises_edited.csv"
+        with open(filePath,'w',newline='') as csvfile:
+            fieldnames = ['Body Part','Exercise']
+            writer = csv.DictWriter(csvfile,fieldnames=fieldnames)
+            print("Delete an avatar")
+            removed_exercise = input("Which exercise to remove?")
+            
+        for row in reader:
+            if removed_exercise != row['Exercise']:
+                writer.writerow(row) ; # write all non-matching rows
+            else:
+                print("Avatar Record Deleted") # nothing to write
+d_avatar()'''
+
+def removeexercise():
+    lines = list()
+    members= input("Which exercise to remove?")
+    with open('/home/laurence/test/exercises.csv', 'r') as readFile:
+        reader = csv.reader(readFile)
+        for row in reader:
+            lines.append(row)
+            for field in row:
+                if field == members:
+                    lines.remove(row)
+    with open('/home/laurence/test/exercises_edited.csv', 'w') as writeFile:
+        writer = csv.writer(writeFile)
+        writer.writerows(lines)
+
+    print(f"Your new exercise lise is:")
+
+    ex = pd.read_csv('/home/laurence/test/exercises_edited.csv',  header=None).groupby([0])[1].agg(list).to_dict()
+    pprint.pprint(ex)
 removeexercise()
