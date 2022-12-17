6da56b3aefcb7ce261d0620e7a4987b096fea072
Laurence-W <laurenceother@hotmail.com>
2022-12-17 20:57:06 +1100

    created workout generator

--- venv/exercisesimport.py
+++ venv/exercisesimport.py
@@ -1,7 +1,5 @@
 import pandas as pd
 import pprint
-from tempfile import NamedTemporaryFile
-import shutil
 import csv
 
 def showexerciselist():
@@ -10,50 +8,6 @@ def showexerciselist():
     #for x,y in (line.rstrip().split(',') for line in f):
         #exercises.setdefault(y, []).append(x)
 showexerciselist()
-
-'''def removeexercise():
-    removed_exercise = str(input("Please type out the exercise to remove:"))
-    filename = '/home/laurence/test/exercises.csv'
-    tempfile = NamedTemporaryFile('w+t', newline='', delete=False)
-
-    with open(filename, 'r', newline='') as csvFile, tempfile:
-        reader = csv.reader(csvFile, delimiter=',')
-        writer = csv.writer(tempfile, delimiter=',')
-
-        for row in reader:
-            if not any(removed_exercise in row):
-                writer.writerow(row)
-
-    shutil.move(tempfile.name, filename)
-removeexercise()'''
-
-'''def removeexercise():
-    removed_exercise = str(input("Please type out the exercise to remove:"))
-    with open('/home/laurence/test/exercises.csv', 'rb') as inp, open('/home/laurence/test/exercises_edited.csv', 'wb') as out:
-        writer = csv.writer(out)
-        for row in csv.reader(inp):
-            if row[1] != removed_exercise:
-                writer.writerow(row)
-removeexercise()'''
-
-'''def d_avatar():
-    filePath = "/home/laurence/test/exercises.csv"
-    with open(filePath)	as csvfile:
-        reader = csv.DictReader(csvfile)
-        
-        filePath = "/home/laurence/test/exercises_edited.csv"
-        with open(filePath,'w',newline='') as csvfile:
-            fieldnames = ['Body Part','Exercise']
-            writer = csv.DictWriter(csvfile,fieldnames=fieldnames)
-            print("Delete an avatar")
-            removed_exercise = input("Which exercise to remove?")
-            
-        for row in reader:
-            if removed_exercise != row['Exercise']:
-                writer.writerow(row) ; # write all non-matching rows
-            else:
-                print("Avatar Record Deleted") # nothing to write
-d_avatar()'''
 
 def removeexercise():
     lines = list()
--- venv/workoutgenerator.py
+++ venv/workoutgenerator.py
@@ -1,42 +1,127 @@
 from datetime import date
-from csv import writer
 import pandas as pd
 import numpy as np
+import pprint
+import csv
+from numpy.random import choice
 #test
-if __name__ == "__main__":
-    days = 0
-    mode = 0
-    #while days not in range(3,7):
-    while not 3 <= days <= 6:
-        days = int(input("How many days would you like to train this week?\n\
-        3  = Push, Pull, Legs\n\
-        4  = Upper, Lower, Upper, Lower\n\
-        5  = Upper, Lower, Push, Pull, Legs\n\
-        6  = Push, Pull, Legs, Push, Pull, Legs\n\
-        Your choice:"
-        ))
-        if not 3 <= days <= 6:
-            print("That is not a valid range. Please input an integer eg. 5")
-    print(f"You have chosen to train {days} times this week")
+#if __name__ == "__main__":
+ex = pd.read_csv('/home/laurence/test/exercises_edited.csv',  header=None).groupby([0])[1].agg(list).to_dict()
+reps = [5,8,10,12,15,20]
+days = 0
+mode = 0
+sets = 0
+#while days not in range(3,7):
+while not 3 <= days <= 6:
+    days = int(input("How many days would you like to train this week?\n\
+    3  = Push, Pull, Legs\n\
+    4  = Upper, Lower, Upper, Lower\n\
+    5  = Upper, Lower, Push, Pull, Legs\n\
+    6  = Push, Pull, Legs, Push, Pull, Legs\n\
+    Your choice:"
+    ))
+    if not 3 <= days <= 6:
+        print("That is not a valid range. Please input an integer eg. 5")
+print(f"You have chosen to train {days} times this week")
 
-    while not 1 <= mode <= 3:
-        mode = int(input("What difficulty level would you like for this workout?\n\
-        1  = Easy\n\
-        2  = Medium\n\
-        3  = Hard\n\
-        Your choice? "))
+while not 1 <= mode <= 3:
+    mode = int(input("What difficulty level would you like for this week?\n\
+    1  = Easy\n\
+    2  = Medium\n\
+    3  = Hard\n\
+    Your choice? "))
 
-        if mode == 1:
-            print("Easy Mode Activated.")
-            #eg.start_workout(type_of_workout, intensity, number_of_exercises, workout_interval)
+    if mode == 1:
+        print("Easy Mode Activated.")
+        sets = 2
+        #eg.start_workout(type_of_workout, intensity, number_of_exercises, workout_interval)
 
-        elif mode == 2:
-            print("Medium Mode Activated.")
-            #eg.start_workout(type_of_workout, intensity, number_of_exercises, workout_interval)
+    elif mode == 2:
+        print("Medium Mode Activated.")
+        sets = 3
+        #eg.start_workout(type_of_workout, intensity, number_of_exercises, workout_interval)
 
-        elif mode == 3:
-            print("Hard Mode? Let's go!")
-            #eg.start_workout(type_of_workout, intensity, number_of_exercises, workout_interval)
+    elif mode == 3:
+        print("Hard Mode? Let's go!")
+        sets = 4
+        #eg.start_workout(type_of_workout, intensity, number_of_exercises, workout_interval)
 
-        else:
-            print("That is not a valid range. Please input an integer eg. 2 for Medium")
+    else:
+        print("That is not a valid range. Please input an integer eg. 2 for Medium")
+
+def day_upper():
+    print("Upper  Body Workout")
+    print(f"Biceps: {choice(ex['biceps'],1,replace=False)} {sets} x {choice(reps,1,replace=False)}")
+    print(f"Triceps: {choice(ex['triceps'],1,replace=False)} {sets} x {choice(reps,1,replace=False)}")
+    print(f"Back Horizontal: {choice(ex['backhorizontal'],1,replace=False)} {sets} x {choice(reps,1,replace=False)}")
+    print(f"Back traps: {choice(ex['backtraps'],1,replace=False)} {sets} x {choice(reps,1,replace=False)}")
+    print(f"Back Vertical: {choice(ex['backvertical'],1,replace=False)} {sets} x {choice(reps,1,replace=False)}")
+    print(f"Chest: {choice(ex['chest'],1,replace=False)} {sets} x {choice(reps,1,replace=False)}")
+    print(f"Chest Fly: {choice(ex['chestfly'],1,replace=False)} {sets} x {choice(reps,1,replace=False)}")
+    print(f"Shoulders Front: {choice(ex['frontdelts'],1,replace=False)} {sets} x {choice(reps,1,replace=False)}")
+    print(f"Shoulders Rear: {choice(ex['reardelts'],1,replace=False)} {sets} x {choice(reps,1,replace=False)}")
+    print(f"Shoulders: {choice(ex['shoulders'],1,replace=False)} {sets} x {choice(reps,1,replace=False)}")
+    print(f"Shoulders Sides: {choice(ex['sidedelts'],1,replace=False)} {sets} x {choice(reps,1,replace=False)}")
+
+def day_lower():
+    print(f"Legs Quads: {choice(ex['legsquads'],1,replace=False)} {sets} x {choice(reps,1,replace=False)}")
+    print(f"Legs Hams: {choice(ex['legshams'],1,replace=False)} {sets} x {choice(reps,1,replace=False)}")
+    print(f"Legs Glutes: {choice(ex['legsglutes'],1,replace=False)} {sets} x {choice(reps,1,replace=False)}")
+    print(f"Legs Calves: {choice(ex['legscalves'],1,replace=False)} {sets} x {choice(reps,1,replace=False)}")
+    print(f"Abdominals: {choice(ex['abdominals'],1,replace=False)} {sets} x {choice(reps,1,replace=False)}")
+
+def day_push():
+    print(f"Chest: {choice(ex['chest'],1,replace=False)} {sets} x {choice(reps,1,replace=False)}")
+    print(f"Chest Fly: {choice(ex['chestfly'],1,replace=False)} {sets} x {choice(reps,1,replace=False)}")
+    print(f"Shoulders Front: {choice(ex['frontdelts'],1,replace=False)} {sets} x {choice(reps,1,replace=False)}")
+    print(f"shoulders: {choice(ex['shoulders'],1,replace=False)} {sets} x {choice(reps,1,replace=False)}")
+    print(f"Shoulders Side: {choice(ex['sidedelts'],1,replace=False)} {sets} x {choice(reps,1,replace=False)}")
+    print(f"Triceps: {choice(ex['triceps'],1,replace=False)} {sets} x {choice(reps,1,replace=False)}")
+
+def day_pull():
+    print(f"Back Horizontal: {choice(ex['backhorizontal'],1,replace=False)} {sets} x {choice(reps,1,replace=False)}")
+    print(f"Back Traps: {choice(ex['backtraps'],1,replace=False)} {sets} x {choice(reps,1,replace=False)}")
+    print(f"Back Vertical: {choice(ex['backvertical'],1,replace=False)} {sets} x {choice(reps,1,replace=False)}")
+    print(f"Biceps: {choice(ex['biceps'],1,replace=False)} {sets} x {choice(reps,1,replace=False)}")
+    print(f"Shoulders Rear: {choice(ex['reardelts'],1,replace=False)} {sets} x {choice(reps,1,replace=False)}")
+
+if days == 3:
+    print("Day 1 Push:")
+    day_push()
+    print("Day 2  Pull:")
+    day_pull()
+    print("Day 3 Lower:")
+    day_lower()
+elif days == 4:
+    print("Day 1 Upper:")
+    day_upper()
+    print("Day 2 Lower:")
+    day_lower()
+    print("Day 3 Upper:")
+    day_upper()
+    print("Day 4 Lower:")
+    day_lower()
+elif days == 5:
+    print("Day 1 Upper:")
+    day_upper()
+    print("Day 2 Lower:")
+    day_lower()
+    print("Day 3 Push:")
+    day_push()
+    print("Day 4 Pull:")
+    day_pull()
+    print("Day 5 Lower:")
+    day_lower()
+elif days == 6:
+    print("Day 1 Push:")
+    day_push()
+    print("Day 2 Pull:")
+    day_pull()
+    print("Day 3 Lower:")
+    day_lower()
+    print("Day 4 Push:")
+    day_push()
+    print("Day 5 Pull:")
+    day_pull()
+    print("Day 6 Lower:")
+    day_lower()
