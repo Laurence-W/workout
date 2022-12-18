92732cf206d99680ddc0101013b904bb57f9de6d
Laurence-W <laurenceother@hotmail.com>
2022-12-18 21:23:51 +1100

    tidy up run script

--- run.sh
+++ run.sh
@@ -1,9 +1,4 @@
 #! /bin/bash
-if ! [[ -x "$(command -v python3)" ]]
-then
-  echo 'Please install Python 3.' >&2
-  exit 1
-fi
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
